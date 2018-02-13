f5a4db4230e6b4f72b2a9c6265c03114b9bedcd2
Champagne1 <e1201583@cmaisonneuve.qc.ca>
2018-02-13 15:02:01 -0500

    Départ

---
+++ 01_app.js
@@ -0,0 +1,37 @@
+var express = require('express');
+var app = express();
+app.use(express.static('public'));
+
+/* on associe le moteur de vue au module «ejs» */
+
+app.set('view engine', 'ejs'); // générateur de template
+
+app.get('/', function (req, res) {
+	let resultat = 
+	[
+		{
+			id : 1,
+			nom : "Eddy Martin",
+			telephone : "514-234-1212"
+		},
+		{
+			id : 1,
+			nom : "Eddy Martin",
+			telephone : "514-234-1212"
+		},
+		{
+			id : 1,
+			nom : "Eddy Martin",
+			telephone : "514-234-1212"
+		}
+	]
+
+	res.render("gabarit1.ejs", {adresses:resultat})
+})
+
+var server = app.listen(8081, function () {
+   var host = server.address().address
+   var port = server.address().port
+   
+   console.log("Example app listening at http://%s:%s", host, port)
+})
