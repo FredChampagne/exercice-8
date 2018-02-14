8ad7aebc81cb794b8339fcb4b7e5bb6b76a89919
Frédéric Champagne <fred.champagne@live.ca>
2018-02-13 22:39:31 -0500

    Formatage et ajout de commentaires

--- 01_app.js
+++ 01_app.js
@@ -1,41 +1,43 @@
+// Éléments qui permettent le fonctionnement de l'application
 const express = require('express');
 const app = express();
 app.use(express.static('public'));
 const MongoClient = require('mongodb').MongoClient;
-const bodyParser= require('body-parser');
+const bodyParser = require('body-parser');
 const ObjectID = require('mongodb').ObjectID;
-/* on associe le moteur de vue au module «ejs» */
-app.set('view engine', 'ejs'); // générateur de template 
-app.use(bodyParser.urlencoded({extended: true}))
-var util = require("util");
+// Associe le moteur de vue au module «ejs» 
+app.set('view engine', 'ejs'); // Générateur de template 
+app.use(bodyParser.urlencoded({ extended: true }))
+let util = require("util");
 
+// Affichage de l'accueil (root)
 app.get('/', function (req, res) {
-		var cursor = db.collection('adresse')
-					   .find().toArray(function(err, resultat){
+	let cursor = db.collection('adresse')
+	.find().toArray(function (err, resultat) {
 		if (err) return console.log(err)
-			console.log('util = ' + util.inspect(resultat));
+		console.log('util = ' + util.inspect(resultat));
 		// transfert du contenu vers la vue index.ejs (renders)
 		// affiche le contenu de la BD
-		res.render('gabarit.ejs', {adresses: resultat})
-		}) 
+		res.render('gabarit.ejs', { adresses: resultat })
+	})
 })
 
 // Traite le formulaire
 app.post('/ajouter', function (req, res) {
 	db.collection('adresse').save(req.body, (err, result) => {
 		if (err) return console.log(err)
-			console.log('sauvegarder dans la BD')
+		console.log('sauvegarder dans la BD')
 		res.redirect('/')
-		})
+	})
 });
 
 let db // variable qui contiendra le lien sur la BD
-// Connection à la base de donnée
+// Connection à la BD
 MongoClient.connect('mongodb://127.0.0.1:27017', (err, database) => {
- if (err) return console.log(err)
- db = database.db('carnet_adresse')
-// lancement du serveur Express sur le port 8081
- app.listen(8081, () => {
- console.log('connexion à la BD et on écoute sur le port 8081')
- })
+	if (err) return console.log(err)
+	db = database.db('carnet_adresse')
+	// lancement du serveur Express sur le port 8081
+	app.listen(8081, () => {
+		console.log('connexion à la BD et on écoute sur le port 8081')
+	})
 })
