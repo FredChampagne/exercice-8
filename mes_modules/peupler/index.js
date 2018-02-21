607c93723e084fc2d858d3f2235df6f4196225c4
Frédéric Champagne <fred.champagne@live.ca>
2018-02-21 17:35:46 -0500

    Génération des membres

--- mes_modules/peupler/index.js
+++ mes_modules/peupler/index.js
@@ -5,14 +5,33 @@ let listeMembres = []
 const peupler_json = () => {
     let position;
 	for(let i=0 ; i<15; i++) {
+        // Génère un numéro de téléphone complet
+        let noTel = "-";
+        for (let j = 0; j < 7; j++) {
+            if(j == 3){
+                noTel += "-";
+            }
+            let unChiffre = genere_NbAleatoire(10);
+            noTel += unChiffre.toString(); 
+        }
+        // Crée le membre
         let unMembre = {};
-        unMembre.nom = tableau.tabNom[Math.floor(Math.random()*tableau.tabNom.length)];
-        unMembre.prenom = tableau.tabPrenom[Math.floor(Math.random()*tableau.tabPrenom.length)];
+        unMembre.nom = tableau.tabNom[genere_NbAleatoire(tableau.tabNom.length)];
+        unMembre.prenom = tableau.tabPrenom[genere_NbAleatoire(tableau.tabPrenom.length)];
+        unMembre.telephone  = tableau.tabIndicatif[genere_NbAleatoire(tableau.tabIndicatif.length)] + noTel;
+        unMembre.courriel = unMembre.nom + "." + unMembre.prenom + "@" + tableau.tabDomaine[genere_NbAleatoire(tableau.tabDomaine.length)];
+        // Formatage du courriel
+        unMembre.courriel = unMembre.courriel.toLowerCase();
+        unMembre.courriel = unMembre.courriel.normalize('NFD').replace(/[\u0300-\u036f]/g, "");
+        unMembre.courriel = unMembre.courriel.replace("-","");
+        // Ajoute le membre dans la liste
         listeMembres.push(unMembre);
 	}
-
 	return(listeMembres);
+}
 
+let genere_NbAleatoire = (max) => {
+    return Math.floor(Math.random()*max)
 }
 
 module.exports = peupler_json;
