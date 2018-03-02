5380b5c66ad0048948a6307a8c94c1f21b63d747
Frédéric Champagne <fred.champagne@live.ca>
2018-03-01 21:55:17 -0500

    Essaies multiples d'afficher la traduction

--- 01_app.js
+++ 01_app.js
@@ -2,7 +2,6 @@
 "use strict";
 const express = require('express');
 const app = express();
-app.use(express.static('public'));
 const MongoClient = require('mongodb').MongoClient;
 const bodyParser = require('body-parser');
 const ObjectID = require('mongodb').ObjectID;
@@ -10,11 +9,11 @@ const i18n = require("i18n");
 const cookieParser = require('cookie-parser')
 const peupler = require("./mes_modules/peupler");
 
-
 // Associe le moteur de vue au module «ejs» 
 app.set('view engine', 'ejs'); // Générateur de template 
 
 // Utilisation
+app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(i18n.init);
 app.use(cookieParser());
@@ -24,6 +23,7 @@ let util = require("util");
 i18n.configure({ 
 	locales : ['fr', 'en'],
 	cookie : 'langueChoisie', 
+	defaultLocale: 'en',
 	directory : __dirname + '/locales' 
 })
 
@@ -34,20 +34,19 @@ app.get('/:locale(en|fr)',  (req, res) => {
 })
 
 // Affichage de l'accueil (root)
-app.get('/', function (req, res) {
-	if (err) return console.log(err);
+app.get('/', (req, res) => {
 	if (req.cookies.langueChoisie == null) {
-		res.cookie('langueChoisie', 'fr ');
+		res.cookie('langueChoisie', 'fr');
 		res.setLocale('fr');
 	}
+	res.render('accueil.ejs');
 })
 
 // Affichage de la liste
-app.get('/list', function (req, res) {
+app.get('/list', (req, res) => {
 	let cursor = db.collection('adresse')
 	.find().toArray(function (err, resultat) {
 		if (err) return console.log(err)
-		// console.log('util = ' + util.inspect(resultat));
 		// transfert du contenu vers la vue index.ejs (renders)
 		// affiche le contenu de la BD
 		res.render('adresses.ejs', { adresses: resultat })
@@ -55,10 +54,8 @@ app.get('/list', function (req, res) {
 })
 
 // Traite le formulaire
-app.post('/modifier', function (req, res) {
-	console.log('req.body' + req.body);
+app.post('/modifier', (req, res) => {
 	if (req.body['_id'] != ""){ 
-		console.log('sauvegarde') 
 		var oModif = {
 			"_id": ObjectID(req.body['_id']), 
 			nom: req.body.nom,
@@ -67,11 +64,8 @@ app.post('/modifier', function (req, res) {
 			courriel:req.body.courriel
 		}
 		var util = require("util");
-		console.log('util = ' + util.inspect(oModif));
 	}
 	else {
-		console.log('insert');
-		console.log(req.body);
 		var oModif = {
 		nom: req.body.nom,
 		prenom:req.body.prenom, 
@@ -98,10 +92,8 @@ app.get('/ajouter/', (req, res) => {
 // Supprime une adresse
 app.get('/detruire/:id', (req, res) => {
 	var id = req.params.id;
-	//console.log(id);
 	// var critere = ObjectID.createFromHexString(id)
 	var critere = ObjectID(id);
-	//console.log(critere);
 	db.collection('adresse').findOneAndDelete({"_id": critere}, (err, resultat) => {
 		if (err) return console.log(err)
 		res.redirect('/list');
@@ -120,12 +112,11 @@ app.get('/trier/:cle/:ordre', (req, res) => {
 });
 
 // Peupler la base de données de membres
-app.get('/peupler', function (req, res) {
+app.get('/peupler', (req, res) => {
 	let peupler = require('./mes_modules/peupler/');
     let listeMembres = peupler();
     db.collection('adresse').insert(listeMembres, (err, resultat) => {
 		if (err) return console.log(err)
-		console.log(listeMembres);
 		listeMembres = [];
         res.redirect('/list');
     });
